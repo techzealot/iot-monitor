ac41d280a5df63eb0ec46dc39ee1f211867ad6c2
techzealot <techzealot@foxmail.com>
2025-01-16 00:31:00 +0800

    init gluestackui with cli

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,8 @@
 import { Stack } from "expo-router";
 
+import "@/global.css";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+
 export default function RootLayout() {
-  return <Stack />;
+  return <GluestackUIProvider mode="light"><Stack /></GluestackUIProvider>;
 }
